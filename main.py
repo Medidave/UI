80eea4572ac34e4e47612c20ec77afdec9646d95
davidawitor23@gmail.com <davidawitor23@gmail.com>
2023-07-12 17:00:50 -0700

    It's going to be a powerful tool

---
+++ main.py
@@ -0,0 +1,24 @@
+#Building a graphic user interface
+
+picture = [
+    [0,0,0,1,0,0,0],
+    [0,0,1,1,1,0,0],
+    [0,1,1,1,1,1,0],
+    [1,1,1,1,1,1,1],
+    [0,0,0,1,0,0,0],
+    [0,0,0,1,0,0,0]
+
+]
+def show_tree():
+    for row in picture:
+        for pixel in row:
+            if (pixel == 1):
+                print('*', end='')
+            else:
+                print(' ', end='')
+        print('')
+show_tree()
+show_tree()
+
+print("My name is The Dave you know")
+
